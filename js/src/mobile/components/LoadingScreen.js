bf43e48ee546e633b140736bebc672d9ea7e1245
Richard David de Sousa Santos <rsforcetecnologia@gmail.com>
2025-09-20 00:03:57 -0300

    voltando ao original

--- js/src/mobile/components/LoadingScreen.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const LoadingScreen = () => (
-    <div className="h-screen w-full flex flex-col items-center justify-center bg-gray-900">
-        <div className="text-2xl font-bold text-white mb-4">Kairos App</div>
-        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-white"></div>
-    </div>
-);
